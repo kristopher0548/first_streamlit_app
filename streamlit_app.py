0e87a42bf9642c01de8740dce7d508aa7f9c316d
kristopher0548 <125303190+kristopher0548@users.noreply.github.com>
2023-02-22 20:24:10 +0800

    Update streamlit_app.py
    Add entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+#display fruit load list
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -42,4 +43,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:") 
 streamlit.dataframe(my_data_row)
+
+
+#Add a Text Entry Box and Send the Input fruit_load_list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', fruit_choice)
 
