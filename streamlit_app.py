d793f4fb0a99d587d83ea912015f60947aaa5ee9
kristopher0548 <125303190+kristopher0548@users.noreply.github.com>
2023-02-22 20:40:28 +0800

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
 #import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice) 
 #take the json version of the response and normalize it 
