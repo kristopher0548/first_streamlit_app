bbd3ef96567caef0c9908879b87fd067be9c8752
kristopher0548 <125303190+kristopher0548@users.noreply.github.com>
2023-02-22 20:41:43 +0800

    Update streamlit_app.py
    fixed indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
 #import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice) 
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice) 
 #take the json version of the response and normalize it 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
